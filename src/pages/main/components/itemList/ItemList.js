479cad721514188c07b01fbb4e9574f074ebb87a
Rey <sooyeon.rok@gmail.com>
2022-06-10 12:08:04 +0900

    ADD: Search Bar 추가
    styled-components 수정
    
    검색 정렬 기능 추가
    
    더보기 기능 추가
    
    scroll back to top 버튼 추가

--- src/pages/main/components/itemList/ItemList.js
+++ src/pages/main/components/itemList/ItemList.js
@@ -1,26 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Item from './Item';
+import SearchBar from './SearchBar';
 import Category from './Category';
 import Banner from './Banner';
 import CATEGORY_LIST from './categoriesListData';
 import BANNER_LIST from './BannerListData';
+import ItemNotFound from './ItemNotFound';
+import SORT_LIST from './SortListData';
 
 const ItemList = () => {
   const [productsList, setProductsList] = useState([]);
+  const [userInput, setUserInput] = useState('');
+  const [optionValue, setOptionValue] = useState('sales');
+  const [limit, setLimit] = useState(8);
+  const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
-    fetch('/data/productsData.json', {
-      method: 'GET',
-    })
+    window.addEventListener('scroll', () => {
+      const isTop = window.scrollY < 500;
+      isTop ? setIsScrolled(false) : setIsScrolled(true);
+    });
+  }, []);
+
+  function handleGoToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  const handleInput = e => {
+    e.preventDefault();
+    setUserInput(e.target.search.value);
+  };
+
+  const OFFSET = 0;
+  const loadQueryString = `&offset=${OFFSET}&limit=${limit}`;
+  const loadMore = () => {
+    if (limit > 64) {
+      alert('상품이 더 이상 없습니다!');
+    } else {
+      setLimit(limit * 2);
+    }
+  };
+
+  useEffect(() => {
+    fetch(
+      `http://13.125.248.213:8000/products?search=${userInput}&sort=${optionValue}${loadQueryString}`,
+      {
+        method: 'GET',
+      }
+    )
       .then(res => res.json())
       .then(data => {
-        setProductsList(data);
+        setProductsList(data.product_list);
       });
-  }, []);
+  }, [userInput, optionValue, loadQueryString]);
 
   return (
     <ContentWrapper>
+      <SearchBar handleInput={handleInput} userInput={userInput} />
       <BannerWrapper>
         <BannerList>
           {BANNER_LIST.map(banner => {
@@ -45,27 +85,52 @@ const ItemList = () => {
         </SearchFilter>
         <ItemContainer>
           <SearchOption>
-            <SortingTitle>
-              <Title>인기순</Title>
-              <SortIcon src="/images/ItemList/up-down.png" />
-            </SortingTitle>
+            <SortingWrapper>
+              <Title
+                onChange={e => {
+                  setOptionValue(e.target.value);
+                }}
+              >
+                {SORT_LIST.map(title => {
+                  return (
+                    <option key={title.id} value={title.value}>
+                      {title.title}
+                    </option>
+                  );
+                })}
+              </Title>
+            </SortingWrapper>
           </SearchOption>
-          <ItemsList>
-            {productsList.map(product => {
-              const { id, productNameEn, productNameKr, price, src } = product;
-              return (
-                <Item
-                  key={id}
-                  productNameEn={productNameEn}
-                  productNameKr={productNameKr}
-                  price={price}
-                  src={src}
-                />
-              );
-            })}
-          </ItemsList>
+          {productsList?.length !== 0 ? (
+            <ItemWrapper>
+              <ItemsList>
+                {productsList.map(product => {
+                  const {
+                    product_id,
+                    eng_name,
+                    kor_name,
+                    thumbnail_url,
+                    price,
+                  } = product;
+                  return (
+                    <Item
+                      key={product_id}
+                      eng_name={eng_name}
+                      kor_name={kor_name}
+                      price={price}
+                      thumbnail_url={thumbnail_url}
+                    />
+                  );
+                })}
+              </ItemsList>
+              <LoadMore onClick={loadMore}>더보기</LoadMore>
+            </ItemWrapper>
+          ) : (
+            <ItemNotFound />
+          )}
         </ItemContainer>
       </Content>
+      {isScrolled && <GoToTopBtn onClick={handleGoToTop}>&uArr;</GoToTopBtn>}
     </ContentWrapper>
   );
 };
@@ -76,7 +141,7 @@ const ContentWrapper = styled.div`
 `;
 
 const BannerWrapper = styled.div`
-  width: 90vw;
+  width: 72rem;
   margin-top: ${props => props.theme.margins.xxxl};
 `;
 
@@ -91,7 +156,7 @@ const Content = styled.div`
 `;
 
 const SearchFilter = styled.div`
-  flex: 1;
+  width: 12rem;
   margin-top: ${props => props.theme.margins.base};
 `;
 
@@ -104,31 +169,61 @@ const Filter = styled.p`
 `;
 
 const ItemContainer = styled.div`
-  flex: 6;
+  margin-left: 3rem;
+  width: 60rem;
 `;
 
 const SearchOption = styled.div``;
 
-const SortingTitle = styled.div`
+const SortingWrapper = styled.div`
   ${props => props.theme.flex.flexBox('_', '_', 'right')}
   padding: 1rem 0;
   padding-right: 1rem;
 `;
 
-const SortIcon = styled.img`
-  width: 1rem;
-  height: 1rem;
+const Title = styled.select`
+  font-size: ${props => props.theme.fontSizes.small};
+  text-align: center;
+  padding: 0.5rem 1rem;
+  margin-right: 0.1rem;
 `;
 
-const Title = styled.p`
-  font-size: ${props => props.theme.fontSizes.small};
-  margin-right: 0.1rem;
+const ItemWrapper = styled.div`
+  ${props => props.theme.flex.flexBox('column')}
 `;
 
 const ItemsList = styled.div`
   display: grid;
   justify-items: center;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: repeat(4, 1fr);
   grid-column-gap: 1rem;
   padding: ${props => props.theme.paddings.base};
+`;
+
+const LoadMore = styled.button`
+  width: 8rem;
+  background: none;
+  border: ${props => props.theme.borders.lightGray};
+  border-radius: 20px;
+  font-weight: ${props => props.theme.fontWeights.thin};
+  font-size: ${props => props.theme.fontSizes.small};
+  padding: 0.9rem 1.5rem;
+  cursor: pointer;
+  :hover {
+    background: lightgray;
+  }
+`;
+
+const GoToTopBtn = styled.button`
+  position: fixed;
+  bottom: 40px;
+  right: 40px;
+  border: 1px solid lightgray;
+  border-radius: 40px;
+  padding: 0.9rem 1rem;
+  background: white;
+  cursor: pointer;
+  :hover {
+    background: lightgray;
+  }
 `;
