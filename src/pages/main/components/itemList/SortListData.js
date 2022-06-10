616ac68a4e7ada8498731f8a04dce90229ed00b9
Rey <sooyeon.rok@gmail.com>
2022-06-10 12:08:04 +0900

    ADD: Search Bar 추가
    styled-components 수정
    
    검색 정렬 기능 추가
    
    더보기 기능 추가
    
    scroll back to top 버튼 추가

---
+++ src/pages/main/components/itemList/SortListData.js
@@ -0,0 +1,29 @@
+const SORT_LIST = [
+  {
+    id: 1,
+    value: 'sales',
+    title: '인기순',
+  },
+  {
+    id: 2,
+    value: 'release_date',
+    title: '발매일순',
+  },
+  {
+    id: 3,
+    value: 'buy_now',
+    title: '즉시 구매가 낮은순',
+  },
+  {
+    id: 4,
+    value: 'sell_now',
+    title: '즉시 판매가 높은순',
+  },
+  {
+    id: 5,
+    value: 'premium',
+    title: '프리미엄순',
+  },
+];
+
+export default SORT_LIST;
