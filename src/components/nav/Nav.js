e522d07e62d332e517b9d973a2b9d233524e2293
seul <kseul06@gmail.com>
2022-06-10 12:45:44 +0900

    Add : Nav 레이아웃 / 폰트설정
    GlobalStyle 변경 및 index.html 구글폰트설정

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import { API } from '../../../src/config.js';
+// import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [isLogin, setIsLogin] = useState(false);
+  const creamToken = localStorage.getItem('cream_token');
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+    creamToken && setIsLogin(true);
+  }, []);
+
+  // 마이페이지 제작 후 처리할 로직
+  // const goToMypage = () => {
+  //   creamToken
+  //     ? fetch(`${API.USERS}`, {
+  //         method: 'GET',
+  //         headers: {
+  //           Authorization: creamToken,
+  //         },
+  //       })
+  //         .then(res => {
+  //           if (res.ok) {
+  //             return res.json();
+  //           }
+  //         })
+  //         .then(() => {
+  //           console.log('');
+  //           navigate('/마이페이지');
+  //         })
+  //     : navigate(`/login`);
+  // };
+
+  return (
+    <Container>
+      <Logo>CREAM</Logo>
+      <ButtonContainer>
+        <Button>STYLE</Button>
+        <Button>SHOP</Button>
+        <Button>ABOUT</Button>
+        {isLogin ? (
+          <Button
+          // onClick={() => {
+          //   goToMypage();
+          // }}
+          >
+            MYPAGE
+          </Button>
+        ) : (
+          <Button login>LOGIN</Button>
+        )}
+      </ButtonContainer>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  ${props => props.theme.flex.flexBox('_', 'center', 'space-between')}
+  padding: 0 ${props => props.theme.paddings.xxxl};
+  height: 4.25rem;
+  border-bottom: 1px solid #cecece;
+`;
+
+const Logo = styled.h1`
+  color: black;
+  font-size: ${props => props.theme.fontSizes.xxl};
+  cursor: pointer;
+`;
+
+const ButtonContainer = styled.div``;
+
+const Button = styled.button`
+  margin: 0 ${props => props.theme.margins.base};
+  border: 0;
+  color: black;
+  font-size: ${props => props.theme.fontSizes.base};
+  background: none;
+  cursor: pointer;
+
+  ${props =>
+    props.login &&
+    `
+  color: gray;
+  `}
+`;
 
 export default Nav;
